be0dace82d60d9d8775fc1f60e392269dfb78bfc
aCrazyCoder <2414637534@qq.com>
2018-09-28 23:17:36 +0800

    修改API文件

--- API/api.py
+++ API/api.py
@@ -214,7 +214,7 @@ class Client(object):
         if len(pictures) == 0:
             return Error.json(Error.Param, 'param pictures is empty')
 
-        requrl = self._conf.build_url('/ocr/namecard')
+        requrl = self._conf.build_url('/ocr/businesscard')
         headers = {
             'Host': self._conf.host(),
             'Authorization': self._auth.get_sign(self._bucket),
@@ -1129,6 +1129,116 @@ class Client(object):
             return Error.json(Error.Param, 'param pictures is empty')
 
         requrl = self._conf.build_url('/ocr/general')
+        headers = {
+            'Host': self._conf.host(),
+            'Authorization': self._auth.get_sign(self._bucket),
+            'User-Agent': Conf.get_ua(self._auth._appid),
+        }
+        files = {
+            'appid': self._auth._appid,
+            'bucket': self._bucket,
+        }
+
+        if isinstance(pictures, CIUrls):
+            headers['Content-Type'] = 'application/json'
+            for url in pictures:
+                files['url'] = url
+            return Http.post(requrl, headers=headers, data=json.dumps(files), timeout=self._conf.timeout())
+
+        else:
+            if isinstance(pictures, CIFiles):
+                i = 0
+                for image in pictures:
+                    if sys.version_info < (3, 0):
+                        filename = urllib.quote(image)
+                        image = image.decode('utf-8')
+                    else:
+                        filename = urllib.parse.quote(image)
+
+                    local_path = os.path.abspath(image)
+                    if not os.path.exists(local_path):
+                        return Error.json(Error.FilePath, 'file ' + image + ' not exists')
+
+                    if not os.path.isfile(local_path):
+                        return Error.json(Error.FilePath, local_path + ' is not file')
+                    files['image'] = (filename, open(image, 'rb'))
+                    i = i + 1
+
+            else:
+                i = 0
+                for buffer in pictures:
+                    files['image'] = buffer
+                    i = i + 1
+
+            return Http.post(requrl, headers=headers, files=files, timeout=self._conf.timeout())
+
+    def handwriting_detect(self, pictures):
+        """ 手写体识别
+        Note:仅识别一张图
+        :param pictures: 需要添加的图片
+        :type  pictures: CIFiles or CIBuffers or CIUrls
+        """
+        # if not isinstance(pictures, CIUrls) and not isinstance(pictures, CIFiles) and not isinstance(pictures, CIBuffers):
+        #     return Error.json(Error.Param, 'param pictures must be instance of CIUrls or CIFiles or CIBuffers')
+        if len(pictures) == 0:
+            return Error.json(Error.Param, 'param pictures is empty')
+
+        requrl = self._conf.build_url('/ocr/handwriting')
+        headers = {
+            'Host': self._conf.host(),
+            'Authorization': self._auth.get_sign(self._bucket),
+            'User-Agent': Conf.get_ua(self._auth._appid),
+        }
+        files = {
+            'appid': self._auth._appid,
+            'bucket': self._bucket,
+        }
+
+        if isinstance(pictures, CIUrls):
+            headers['Content-Type'] = 'application/json'
+            for url in pictures:
+                files['url'] = url
+            return Http.post(requrl, headers=headers, data=json.dumps(files), timeout=self._conf.timeout())
+
+        else:
+            if isinstance(pictures, CIFiles):
+                i = 0
+                for image in pictures:
+                    if sys.version_info < (3, 0):
+                        filename = urllib.quote(image)
+                        image = image.decode('utf-8')
+                    else:
+                        filename = urllib.parse.quote(image)
+
+                    local_path = os.path.abspath(image)
+                    if not os.path.exists(local_path):
+                        return Error.json(Error.FilePath, 'file ' + image + ' not exists')
+
+                    if not os.path.isfile(local_path):
+                        return Error.json(Error.FilePath, local_path + ' is not file')
+                    files['image'] = (filename, open(image, 'rb'))
+                    i = i + 1
+
+            else:
+                i = 0
+                for buffer in pictures:
+                    files['image'] = buffer
+                    i = i + 1
+
+            return Http.post(requrl, headers=headers, files=files, timeout=self._conf.timeout())
+
+    def bankcard_detect(self, pictures):
+        """ 银行卡识别
+        Note:仅识别一张图
+        :param pictures: 需要添加的图片
+        :type  pictures: CIFiles or CIBuffers or CIUrls
+        """
+        # if not isinstance(pictures, CIUrls) and not isinstance(pictures, CIFiles) and not isinstance(pictures, CIBuffers):
+        #     return Error.json(Error.Param, 'param pictures must be instance of CIUrls or CIFiles or CIBuffers')
+        if len(pictures) == 0:
+            return Error.json(Error.Param, 'param pictures is empty')
+
+        requrl = self._conf.build_url('/ocr/bankcard')
         headers = {
             'Host': self._conf.host(),
             'Authorization': self._auth.get_sign(self._bucket),
